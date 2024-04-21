e4f68b0a855a090acf6bef86e169e9a7f1905eb1
Alex Mirrlees-Black <tmbx100@gmail.com>
2024-04-22 01:32:32 +1000

    Add key remaps to nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -14,3 +14,33 @@ vim.opt.mouse = 'a'
 vim.opt.termguicolors = true
 vim.opt.colorcolumn = { 80 }
 
+vim.g.mapleader = ","
+vim.g.maplocalleader = "\\"
+
+function map(mode, shortcut, command)
+  vim.api.nvim_set_keymap(mode, shortcut, command, {noremap = true, silent = true})
+end
+
+function nmap(shortcut, command)
+  map('n', shortcut, command)
+end
+
+function imap(shortcut, command)
+  map('i', shortcut, command)
+end
+
+nmap("<C-S>", ":nohlsearch<C-R>")
+imap("<C-S>", "<ESC>:nohlsearch<C-R>i")
+nmap("j", "gj")
+nmap("k", "gk")
+
+for k, v in ipairs({'J', 'K', 'L', 'H'}) do
+  nmap("<C-" .. v .. ">", "<C-W><C-" .. v .. ">")
+end
+
+nmap("Y", "y$")
+nmap("<C-Y>", "\"+y")
+nmap("<C-P>", "\"+p")
+
+
+
