92cc939f8cc70bd2ce24afa7dd655ce22574b022
Alex Mirrlees-Black <tmbx100@gmail.com>
2024-04-22 01:13:23 +1000

    Initial neovim config

---
+++ config/nvim/init.lua
@@ -0,0 +1,16 @@
+vim.opt.number = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.scrolloff = 3
+vim.opt.sidescrolloff = 3
+vim.opt.cursorline = true
+vim.opt.linebreak = true
+vim.opt.showmode = false
+vim.opt.showcmd = false
+vim.opt.mouse = 'a'
+vim.opt.termguicolors = true
+vim.opt.colorcolumn = { 80 }
+
